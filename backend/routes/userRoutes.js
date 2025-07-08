5c7651143045bfd4a85676e8447ebdecfa9a7f2f
Elaya1220 <elayaraja1220@outlook.com>
2025-07-08 17:52:01 +0530

    fullstack-expressjs

--- backend/routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const userControllers = require('../controllers/userControllers');
-
-//router.post('/send-otp', userControllers.sendOtp);
-//router.post('/verify-otp', userControllers.verifyOtpAndCreateUser);
-router.post('/users', userControllers.userDetails)
-module.exports = router;
